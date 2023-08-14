699deb98492cdd6657cdf680d2add8bdb709edba
jmcshan1 <jmcshan01@gmail.com>
2023-08-13 20:20:11 -0500

    added parent class for shapes and individual child classes

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
 
+// const svgLayout =` 
+// <svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+
+//   <circle cx="150" cy="100" r="80" fill="green" />
+
+//   <text x="150" y="125" font-size="60" text-anchor="middle" fill="white">SVG</text>
+
+// </svg>`
+
 inquirer
     .prompt([
         {
@@ -25,3 +34,6 @@ inquirer
             name: 'svgShapeColor',
         },
     ])
+    .then((data) => {
+        console.log(data)
+    })
